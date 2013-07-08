e714375963fcebfb48b74057ba91ec029617dc1c
Hwangss <mssf2e@163.com>
2013-07-09 01:14:01 +0800

    Update resume.js

--- category/js/resume.js
+++ category/js/resume.js
@@ -1,27 +1,49 @@
-//判断是否支持某CSS属性
+//this page
+var mss = {
+	cover: function() {
+		//若能执行动画则等待开场动画结束，否则就直接淡出
+		var wrap = get("coverWrap");
+		var ele = get("cover");
+		var main=get("main");
+		var bAnimation = cssCanUse(["-webkit-animation", "-moz-animation", "-o-animation", "-ms-animaition", "animation"])
+		var coverSte;
+		show(wrap)
+		if (bAnimation) {
+			coverStep = 0;
+			addEvent(ele, "oanimationend", function() {
+				coverStep++;
+				coverStep == 4 && function() {
+					hide(wrap);
+					show(main);
+					normalMove(main,{"opacity":"100"});
+				}()
 
-function cssCanUse(arr) {
-  var element = document.createElement('div');
-	var flag = false;
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] in element.style) {
-			flag = true;
+			})
+			addEvent(ele, "animationend", function() {
+				coverStep++;
+				coverStep == 4 && function() {
+					hide(wrap);
+					show(main);
+					normalMove(main,{"opacity":"100"});
+				}()
+			})
+			addEvent(ele, "webkitAnimationEnd", function() {
+				coverStep++;
+				coverStep == 4 && function() {
+					hide(wrap);
+					show(main);
+					normalMove(main,{"opacity":"100"});
+				}();
+			})
+		} else {
+			setTimeout(function() {
+				hide(wrap);
+				show(main);
+			}, 2000)
 		}
+	},
+	init: function() {
+		this.cover()
 	}
-	return flag;
 }
-
-$(function() {
-	//若能执行动画则等待开场动画结束，否则就直接淡出
-	var bAnimation = cssCanUse(["-webkit-animation", "-moz-animation", "-o-animation", "-ms-animaition", "animation"])
-	var coverSte;
-	if (bAnimation) {
-		coverStep = 0;
-		$("#cover").bind('oanimationend animationend webkitAnimationEnd', function() {
-			coverStep++;
-			coverStep == 4 && $(this).parent().hide();
-		});
-	} else {
-		$("#cover").parent().fadeOut(3000)
-	}
-})
+mss.init()
